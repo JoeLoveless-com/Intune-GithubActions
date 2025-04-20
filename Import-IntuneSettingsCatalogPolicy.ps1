7a2d81f4f02f303186390ebaa277c830151feb5a
Joe Loveless <jloveless03@pm.me>
2025-04-20 07:08:49 -0500

    4.20_test1

--- Import-IntuneSettingsCatalogPolicy.ps1
+++ Import-IntuneSettingsCatalogPolicy.ps1
@@ -1,71 +1,27 @@
-<#
-.SYNOPSIS
-Creates (or updates if exist) device health scripts in Intune.
-.DESCRIPTION
-This script searches in a provided folder for subfolders where the scripts are in. 
-The script name is the name of the folder where the script set is in. In the script set folder there must be a detection_xx.ps1 and remediate_xx.ps1
-.PARAMETER GraphToken
-Enter the Graph Bearer token
-.PARAMETER ScriptsFolder
-Provide the path where the scripts folders are.
-.EXAMPLE
-.\manage-devicehealthscripts.ps1 -GraphToken xxxx -ScriptsFolder .\AllDetectionScripts
-#>
-[CmdletBinding()]
+function Import-IntuneSettingsCatalogPolicy {
+
+
+    [CmdletBinding()]
     param
     (
-       [parameter(Mandatory)]
-       [ValidateNotNullOrEmpty()]
-       [string]$GraphToken,
-
         [parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [string]$Folder
+        [string]$folder
     )
 
-    $GraphTokenResponse = az account get-access-token --resource https://graph.microsoft.com
-$GraphToken = ($GraphTokenResponse | ConvertFrom-Json).accessToken
+    #Variables
+    $scopes = "DeviceManagementConfiguration.ReadWrite.All"
+    $policyfiles = Get-ChildItem $folder | Select-Object -ExpandProperty Name
 
-try {
-    $folders = Get-ChildItem -Path $folder -Directory
-    $headers = @{
-        "Content-Type" = "application/json"
-        Authorization = "Bearer {0}" -f $GraphToken
-    }
-    $apiUrl = "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies"
-    $method = "POST"
+    Foreach ($policyfile in $policyfiles) {
 
-    foreach ($folder in $folders) {
-        try {
-            $policy = Invoke-webrequest -Uri $apiUrl -Method GET -Headers $headers
-            $policyCheck = ($policy.content | Convertfrom-json).value | Where-Object {$_.displayName -eq $folder.Name}
-
-            $jsonFile = Get-ChildItem -Path $folder.FullName -File -Filter "*.Json"
-            if ($jsonFile) {
-                Write-Host "File found: $($jsonFile.Name)"
-                $jsonContent = get-content $jsonFile
-                $jsonConvert = $jsonContent | ConvertFrom-Json
-                $DisplayName = $jsonConvert.name
-                $jsonOutput = $jsonConvert | ConvertTo-Json -depth 20
-            } else {
-                Write-Host "No Settings Catalog file found."
-            }
-
-            Write-Host "Settings Catalog Policy '$DisplayName' Found..."
-            $jsonOutput
-            Write-Host "Adding Settings Catalog Policy '$DisplayName'"
-            Try{
-            Invoke-webrequest -Uri $apiUrl -Method $Method -Headers $headers -Body $jsonOutput
-            }
-            Catch{
-                Write-Host "$_"
-            }
+        Try {
+            $policy = Get-Content -path $folder\$policyfile
+            Invoke-MgGraphRequest -Method POST https://graph.microsoft.com/beta/deviceManagement/configurationPolicies -ContentType "application/json" -Body $policy
         }
-        catch {
-            Write-Host "Not able to create policy with name $($folder.name), $_"
+        Catch {
+            Write-Host "there was an error importing $policyfile"
+            Write-Host $_
         }
     }
-}
-catch {
-    Write-Host "Not able to run succesfully, $_"
 }
