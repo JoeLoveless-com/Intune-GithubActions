078c3c1c258413a410ce29dd372c2ee5f2fcc585
Joe Loveless <jloveless03@pm.me>
2025-04-20 08:57:13 -0500

    4.20_test11

--- Import-IntuneSettingsCatalogPolicy.ps1
+++ Import-IntuneSettingsCatalogPolicy.ps1
@@ -4,18 +4,25 @@ function Import-IntuneSettingsCatalogPolicy {
     (
         [parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
+        [string]$GraphToken,
+        [parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
         [string]$folder
     )
 
-    #Variables
-    #$scopes = "DeviceManagementConfiguration.ReadWrite.All"
+    $headers = @{
+        "Content-Type" = "application/json"
+        Authorization = "Bearer {0}" -f $GraphToken
+    }
+
     $policyfiles = Get-ChildItem $folder | Select-Object -ExpandProperty Name
 
     Foreach ($policyfile in $policyfiles) {
 
         Try {
             $policy = Get-Content -path $folder\$policyfile
-            Invoke-MgGraphRequest -Method POST https://graph.microsoft.com/beta/deviceManagement/configurationPolicies -ContentType "application/json" -Body $policy
+            Invoke-MgGraphRequest -Method POST https://graph.microsoft.com/beta/deviceManagement/configurationPolicies -ContentType "application/json" -Body $policy -headers $headers
+            
         }
         Catch {
             Write-Host "there was an error importing $policyfile"
