a9a6fa66ed8277db6d97db3e7c7f08af31370544
Joe Loveless <jloveless03@pm.me>
2025-04-21 20:25:52 -0500

    test

--- Import-IntuneSettingsCatalogPolicy.ps1
+++ Import-IntuneSettingsCatalogPolicy.ps1
@@ -3,37 +3,26 @@ param
 (
     [parameter(Mandatory)]
     [ValidateNotNullOrEmpty()]
-    [string]$GraphToken,
-    [parameter(Mandatory)]
-    [ValidateNotNullOrEmpty()]
     [string]$folder
 )
 
-$headers = @{
-    "Content-Type" = "application/json"
-    Authorization = "Bearer {0}" -f $GraphToken
-}
-
 $policyfiles = Get-ChildItem $folder | Select-Object -ExpandProperty Name
 
-Foreach ($policyfile in $policyfiles) {
-
-    Try {
+Foreach ($policyfile in $policyfiles){
+    try{
         $policy = Get-Content -path $folder\$policyfile
-        $policyCheck = (Invoke-webrequest -Uri "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies" -Method GET -Headers $headers).value
+        $policyCheck = (Invoke-Mggraphrequest -uri "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies" -Method GET).value
 
         if ($policyCheck.Name -gt 0){
         Write-Host "$($policyCheck.Name) already exists, patching profile"
-        Invoke-webrequest -Uri "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies" -Method PATCH -Headers $headers -Body $policy
+        Invoke-Mggraphrequest -Uri "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies" -Method PATCH -Body $policy
         }
         if (!$policyCheck.Name -lt 1){
             Write-Host "$($policyCheck.Name) does not exist, creating new profile"
-            Invoke-webrequest -Uri "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies" -Method POST -Headers $headers -Body $policy
+            Invoke-Mggraphrequest -Uri "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies" -Method POST -Body $policy
         }
-    }
-    Catch {
-        Write-Host "there was an error importing $policyfile"
-        Write-Host $_
-    }
 }
-
+Catch{
+    Write-Error "Error: $_"
+}
+}
