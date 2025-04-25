762e23eb1569ce35a90f150a0acfcaa71fddc3ad
Joe Loveless <jloveless03@pm.me>
2025-04-24 20:59:49 -0500

    new

--- Import-IntuneSettingsCatalogPolicy.ps1
+++ Import-IntuneSettingsCatalogPolicy.ps1
@@ -1,44 +1,27 @@
 [CmdletBinding()]
-param (
+param
+(
     [parameter(Mandatory)]
     [ValidateNotNullOrEmpty()]
     [string]$folder
 )
 
+
 $policyfiles = Get-ChildItem $folder | Select-Object Name, BaseName
 
-foreach ($policyfile in $policyfiles) {
+Foreach ($policyfile in $policyfiles){
     $policyName = $policyfile.Name
-    $policyBaseName = $policyfile.BaseName
-    $policyRaw = Get-Content -Path "$folder\$policyName" -Raw
+    $policybaseName = $policyfile.BaseName
 
-    # Convert to PowerShell object
-    $policyObj = $policyRaw | ConvertFrom-Json
+        $policy = Get-Content -path $folder\$policyName
+        $policyCheck = (Invoke-Mggraphrequest -uri "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies" -Method GET).value | Where-Object { $_.Name -eq $policyBaseName }
 
-    # Remove problematic navigation annotations
-    $policyObj.PSObject.Properties.Remove('settingDefinitions@odata.associationLink')
-
-    # Convert back to JSON
-    $policyJson = $policyObj | ConvertTo-Json -Depth 10
-
-    $policyCheck = @()
-    $uri = "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies"
-
-    do {
-        $response = Invoke-MgGraphRequest -Uri $uri -Method GET
-        $policyCheck += $response.value
-
-        # Pagination
-        $uri = $response.'@odata.nextLink'
-    } while ($uri)
-
-    $existingPolicy = $policyCheck | Where-Object { $_.Name -eq $policyBaseName }
-
-    if ($existingPolicy) {
-        Write-Host "$($existingPolicy.Name) already exists, modifying profile with PUT"
-        Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies/$($existingPolicy.Id)" -Method PUT -Body $policyJson -ContentType "application/json"
-    } else {
-        Write-Host "$policyBaseName does not exist, creating new profile"
-        Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies" -Method POST -Body $policyJson -ContentType "application/json"
+        if ($policyCheck.Name){
+        Write-Host "$($policyCheck.Name) already exists, modifying profile with PUT"
+        $put = Invoke-Mggraphrequest -Uri "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies/$($policyCheck.Id)" -Method PUT -Body $policy -ContentType "application/json"
+        }
+        else{
+            Write-Host "$policybaseName does not exist, creating new profile"
+            $post = Invoke-Mggraphrequest -Uri "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies" -Method POST -Body $policy -ContentType "application/json"
     }
 }
